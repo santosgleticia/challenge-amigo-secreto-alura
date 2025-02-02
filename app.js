956a0f98850cff4d0a515e246d0c0e475baf7f28
Letícia Santos <santos.g.leti@gmail.com>
2025-02-02 12:08:30 -0300

    função atualizar lista de amigos criada

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ let listaDeAmigos = [];
 function adicionarAmigo() {
     let amigoAdicionado = document.querySelector('input').value;
 
-    if (amigoAdicionado == '') {
+    if(amigoAdicionado == '') {
         alert('Por favor, insira um nome.');
     } else {
         listaDeAmigos.push(amigoAdicionado);
+        console.log(listaDeAmigos);
     }
 
     amigoAdicionado = document.querySelector('input');
     amigoAdicionado.value = '';
+
+    atualizarListaDeAmigos();
+}
+
+function atualizarListaDeAmigos() {
+    let exibicaoListaAmigos = document.getElementById('listaAmigos');
+    exibicaoListaAmigos.innerHTML = '';
+
+    for(i = 0; i <= listaDeAmigos.length - 1; i++) {
+        let novoAmigo = document.createElement('li');
+        novoAmigo.textContent = listaDeAmigos[i];
+        exibicaoListaAmigos.appendChild(novoAmigo);
+    }
 }
